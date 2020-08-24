6c49cb6f02095d4ee60893449a3f853b4e3290cc
Erik Andersson <Anderzzon_91@hotmail.com>
2020-08-24 23:09:20 +0200

    implemented viewpager to MainActivity and a tablayout prototype

--- app/src/main/java/com/dankout/chipstr/MainActivity.java
+++ app/src/main/java/com/dankout/chipstr/MainActivity.java
@@ -2,13 +2,58 @@ package com.dankout.chipstr;
 
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
-public class MainActivity extends AppCompatActivity {
+import com.dankout.chipstr.ui.UserFragment;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayout.Tab;
+import com.google.android.material.tabs.TabLayoutMediator;
+import com.google.android.material.tabs.TabLayoutMediator.TabConfigurationStrategy;
+
+public class MainActivity extends FragmentActivity {
+
+    private static final int NUMBER_OF_PAGES = 3;
+    private ViewPager2 mPager;
+    private FragmentStateAdapter mPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.user_fragment);
+        setContentView(R.layout.activity_main);
+
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        mPager = findViewById(R.id.view_pager);
+        mPagerAdapter = new MainScreenPagerAdapter(this);
+        mPager.setAdapter(mPagerAdapter);
+
+        new TabLayoutMediator(tabLayout, mPager, true, new TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull Tab tab, int position) {
+                tab.setText("Page " + position);
+            }
+        }).attach();
     }
+
+    private class MainScreenPagerAdapter extends FragmentStateAdapter {
+        public MainScreenPagerAdapter(FragmentActivity fragmentActivity) {
+            super(fragmentActivity);
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            return new UserFragment();
+        }
+
+        @Override
+        public int getItemCount() {
+            return NUMBER_OF_PAGES;
+        }
+    }
+
+
 }
