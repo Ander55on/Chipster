31d5591155a50964185bc1212b0427d556cd8c30
Erik Andersson <Anderzzon_91@hotmail.com>
2020-08-27 19:11:30 +0200

    prototyped a hole_fragment layout

--- app/src/main/java/com/dankout/chipstr/RoundActivity.java
+++ app/src/main/java/com/dankout/chipstr/RoundActivity.java
@@ -5,11 +5,24 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentActivity;
 
+import com.dankout.chipstr.ui.HoleFragment;
+
 public class RoundActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.round_activity);
+
+        if(findViewById(R.id.fragment_container) != null) {
+           //To prevent multiple fragments overlapping
+            if(savedInstanceState != null) {
+                return;
+            }
+
+            HoleFragment fragment = new HoleFragment();
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, fragment).commit();
+
+        }
     }
 }
